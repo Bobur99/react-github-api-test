29572ba135723207db9b2421db41500b5ac8176b
coder <coder.pro@mail.ru>
2022-03-27 00:37:34 +0500

    Updates

---
+++ precache-manifest.24b131f98300ec66b7b2668bb6e0570c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4657d925712192c0249fc184dfe33af8",
+    "url": "/react-github-api-test/index.html"
+  },
+  {
+    "revision": "f7ad0aedd5248386cdaa",
+    "url": "/react-github-api-test/static/css/main.dc185e9d.chunk.css"
+  },
+  {
+    "revision": "344e1cf779f70eaf13c8",
+    "url": "/react-github-api-test/static/js/2.08904dbb.chunk.js"
+  },
+  {
+    "revision": "f7ad0aedd5248386cdaa",
+    "url": "/react-github-api-test/static/js/main.b8087703.chunk.js"
+  },
+  {
+    "revision": "1d2ade380f2538ff20aa",
+    "url": "/react-github-api-test/static/js/runtime~main.0c528d4a.js"
+  }
+]);
